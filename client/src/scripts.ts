e2a2bc43546cd24588527c9d9a8cebff0eb3402e
SuperC <clarkbros3@gmail.com>
2020-07-25 03:08:40 +0200

    Added Common Folder for Shared Code

--- client/src/scripts.ts
+++ client/src/scripts.ts
@@ -1,6 +1,8 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 
+import { ERequestCode } from '../../common/src';
+
 // Initialize UI-Kit
 // @ts-ignore
 UIkit.use(Icons);
---
+++ common/code.enum.ts
@@ -0,0 +1,7 @@
+export enum ERequestCode {
+  
+}
+
+export enum EResponseCode {
+  
+}
